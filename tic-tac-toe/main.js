a93575ba242ec0d145f0a2df5f8a5b4810e698f8
Valda <valda.bimbirule1@gmail.com>
2023-10-15 07:20:24 +0300

    Creating board

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -1,5 +1,17 @@
 const gameBoard = document.querySelector("#gameboard");
 const infoDisplay = document.querySelector("#info");
 const startCells = [
-    
+    "", "", "",
+    "", "", "",
+    "", "", ""
 ];
+
+function createBoard(){
+    startCells.forEach((cell, index) => {
+        const cellElement = document.createElement("div");
+        cellElement.classList.add("square");
+        gameBoard.append(cellElement);
+    })
+}
+
+createBoard();

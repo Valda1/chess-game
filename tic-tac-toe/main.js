e8fed323af7e129187aa749783792126e21a4571
Valda <valda.bimbirule1@gmail.com>
2023-10-15 08:19:26 +0300

    Creating O's and X's

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -6,12 +6,28 @@ const startCells = [
     "", "", ""
 ];
 
+let go = "cicrle";
+infoDisplay.textContent = "Circle goes first!";
+
 function createBoard(){
-    startCells.forEach((cell, index) => {
+    startCells.forEach((_cell, index) => {
         const cellElement = document.createElement("div");
         cellElement.classList.add("square");
+        cellElement.id = index;
+        cellElement.addEventListener("click", addGo);
         gameBoard.append(cellElement);
     })
 }
 
 createBoard();
+
+function addGo(event){
+    //console.log(event.target);
+    const goDisplay = document.createElement("div");
+    goDisplay.classList.add(go);
+    event.target.append(goDisplay);
+    go = go === "circle" ? "cross" : "circle"
+    infoDisplay.textContent = "It is now " + go + "'s go!";
+    event.target.removeEventListener("click", addGo);
+}
+

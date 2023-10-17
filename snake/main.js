e23ba76e60449b66bbaad2fbe9a145ae07bb7516
Valda <valda.bimbirule1@gmail.com>
2023-10-17 21:45:31 +0300

    Game over conditions

--- snake/main.js
+++ snake/main.js
@@ -18,6 +18,8 @@ var snakeBody = [];
 var foodX;
 var foodY;
 
+var gameOver = false;
+
 window.onload = function(){
     board = document.getElementById("board");
     board.height = rows * blockSize;
@@ -31,6 +33,10 @@ window.onload = function(){
 }
 
 function update(){
+    if(gameOver){
+        return;
+    }
+
     context.fillStyle = "black";
     context.fillRect(0, 0, board.width, board.height);
 
@@ -56,6 +62,19 @@ function update(){
     context.fillRect(snakeX, snakeY, blockSize, blockSize);
     for(let i = 0; i < snakeBody.length; i++){
         context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+    }
+
+    //game over conditions
+    if(snakeX < 0 || snakeX > cols * blockSize || snakeY < 0 || snakeY > rows * blockSize){
+        gameOver = true;
+        alert("Game over! You lose!");
+    }
+
+    for(let i = 0; i < snakeBody.length; i++){
+        if(snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]){
+            gameOver = true;
+            alert("Game over! You lose!");
+        }
     }
 }
 

6031af7139533bf2d2c66ffe976567585c5974c9
Valda <valda.bimbirule1@gmail.com>
2023-10-19 07:25:03 +0300

    Moving paddle

--- breakeout/main.js
+++ breakeout/main.js
@@ -5,12 +5,14 @@ let context;
 
 let playerHeight = 10;
 let playerWidth = 80;
+let playerVelocityX = 10;
 
 let player = {
     x: boardWidth / 2 - playerWidth / 2,
     y: boardHeight - playerHeight - 5,
     width: playerWidth,
-    height: playerHeight
+    height: playerHeight,
+    velocityX: playerVelocityX
 }
 
 window.onload = function(){
@@ -24,13 +26,35 @@ window.onload = function(){
     context.fillRect(player.x, player.y, player.width, player.height);
 
     requestAnimationFrame(update);
+    document.addEventListener("keydown", movePlayer);
 }
 
 function update(){
     requestAnimationFrame(update);
+
+    context.clearRect(0, 0, board.width, board.height);
+
     context.fillStyle = "lightgreen";
     context.fillRect(player.x, player.y, player.width, player.height);
 }
 
+function outOfBounds(xPosition){
+    return (xPosition < 0 || xPosition + playerWidth > boardWidth);
+}
 
+function movePlayer(event){
+    if(event.code == "ArrowLeft"){
+        //player.x -= player.velocityX;
+        let nextPlayerX = player.x - player.velocityX;
+        if(!outOfBounds(nextPlayerX)){
+            player.x = nextPlayerX;
+        }
+    }else if(event.code == "ArrowRight"){
+        //player.x += playerVelocityX;
+        let nextPlayerX = player.x + player.velocityX;
+        if(!outOfBounds(nextPlayerX)){
+            player.x = nextPlayerX;
+        }
+    }
+}
 
